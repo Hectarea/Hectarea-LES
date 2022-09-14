47fc71daa7dc1c02cc412c3c0749143eefc1355d
Hectarea <gonzalez.craqnell@gmail.com>
2022-09-14 18:52:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,4 +17,5 @@ setup(
     author_email = 'gonzalez.craqenll@gmail.com',
     url = 'https://hectarea.netlify.app/',
     keywords = ['Encryption', 'Dictionary', 'Hectarea', 'Secrets'],
-    download_url= 'https://github.com/Hectarea/DictionaryEncryption/archive/refs/tags/4.20.68.2.tar.gz',)
+   ,)
+ #download_url= 'https://github.com/Hectarea/DictionaryEncryption/archive/refs/tags/4.20.68.2.tar.gz'
